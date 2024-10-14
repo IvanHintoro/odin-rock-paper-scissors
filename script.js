9051996711c28eb058f4c6d345fd8f3488f64712
IvanHintoro <ivanhintoro@gmail.com>
2024-10-14 10:50:21 +0700

    Added playRound function

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 function getComputerChoice() {
     let rng = Math.random();
     if (rng < 0.333){
-        return("Rock");
+        return("rock");
     }
     else if (rng < 0.667) {
-        return("Paper");
+        return("paper");
     }
     else {
-        return("Scissors");
+        return("scissors");
     }
 };
+
+function getHumanChoice() {
+    let playerChoice = prompt("Rock, Paper, or Scissors?");
+    return (playerChoice.toLowerCase());
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    console.log(`You pick ${humanChoice}`);
+    console.log(`Computer used ${computerChoice}`);
+
+    if (humanChoice == computerChoice){
+        console.log("It's a draw");
+    }
+    else if (
+        (humanChoice == "rock" && computerChoice == "scissors") ||
+        (humanChoice == "paper" && computerChoice == "rock") ||
+        (humanChoice == "scissors" && computerChoice == "paper")
+    ) {
+        console.log("Congratulations! You win")
+    }
+    else {
+        console.log("Unfortunately, you lose")
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
